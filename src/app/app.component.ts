5a27cf5366f2a25350e4e23869cc92ad04cabd11
Omkar Shinde <Omkar.shinde@inquizity.com>
2023-02-14 15:45:31 +0530

    second change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  title="Omakr Shinde"
   loadComponent:string='recipe'
   onClick(featured:string){
     this.loadComponent=featured
